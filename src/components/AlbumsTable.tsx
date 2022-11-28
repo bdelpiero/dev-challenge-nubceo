055397e54eb5778b26f91f8fb2f48692b0f63199
Bruno Del Piero <bdelpiero93@gmail.com>
2022-11-28 11:46:10 -0300

    Add albums table to band page

---
+++ src/components/AlbumsTable.tsx
@@ -0,0 +1,52 @@
+import { TableContainer, Table, Thead, Tr, Th, Tbody, Td, Spinner } from '@chakra-ui/react'
+import { API_URL, API_ENDPOINTS, CONTAINER_MAX_WIDTH, BAND_DETAILS } from '../constants'
+import useFetch from '../hooks/useFetch'
+import { IAlbum } from '../types'
+
+export function AlbumsTable({ bandId }: { bandId: number }) {
+  const { data: albums, error } = useFetch<IAlbum[]>(
+    `${API_URL}/${API_ENDPOINTS.albums}?bandId=${bandId}`
+  )
+
+  if (error) {
+    return <p>{`Error fetching albums: ${error}`}</p>
+  }
+
+  if (!albums) {
+    return <Spinner />
+  }
+
+  return (
+    <Table
+      alignSelf={'center'}
+      size={'md'}
+      maxW={['90%', '600px']}
+      margin={'auto'}
+      mt={'50px !important'}
+      border={'1px'}
+      borderColor={'gray.300'}
+      borderRadius={10}
+      height={'fit-content'}
+    >
+      <Thead backgroundColor={'black'}>
+        <Tr>
+          <Th color={'white'}>Album</Th>
+          <Th color={'white'}>Year</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {!albums.length ? (
+          <Td>No albums found</Td>
+        ) : (
+          albums.map((album) => (
+            // API error: should use album id as key but all ids are set to 1
+            <Tr key={album.name + bandId}>
+              <Td>{album.name}</Td>
+              <Td>{album.year}</Td>
+            </Tr>
+          ))
+        )}
+      </Tbody>
+    </Table>
+  )
+}
--- src/pages/Band.tsx
+++ src/pages/Band.tsx
@@ -1,39 +1,47 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Heading, Spinner, VStack } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
-import { API_ENDPOINTS, API_URL } from '../constants'
+import { API_ENDPOINTS, API_URL, BAND_DETAILS } from '../constants'
 import { IAlbum } from '../types'
 import useFetch from '../hooks/useFetch'
 import { useFetchBandById } from '../hooks/useFetchBandById'
-
-export function Albums({ bandId }: { bandId: number }) {
-  const { data: albums, error } = useFetch<IAlbum[]>(
-    `${API_URL}/${API_ENDPOINTS.albums}?bandId=${bandId}`
-  )
-
-  // TODO: add loader
-  return (
-    <Flex>
-      {albums?.map((album) => (
-        // API error: should use album id as key but all ids are set to 1
-        <p key={`${bandId}-${album.name}`}>{album.name}</p>
-      ))}
-    </Flex>
-  )
-}
+import { Detail } from '../components/Detail'
+import { CONTAINER_MAX_WIDTH } from '../constants'
+import { AlbumsTable } from '../components/AlbumsTable'
 
 export function Band() {
   const { id } = useParams()
   const { band, error } = useFetchBandById(id)
 
+  // TODO: create Error component
+  if (error) {
+    return <p>{`Error fetching band details: ${error}`}</p>
+  }
   // TODO: loader
-  if (!band) return null
-
-  console.log(band, 'band')
+  if (!band) return <Spinner />
 
   return (
-    <>
-      <p>{band.name}</p>
-      <Albums bandId={band.id} />
-    </>
+    <VStack
+      border={['none', '1px']}
+      borderColor={['', 'gray.300']}
+      borderRadius={10}
+      maxW={CONTAINER_MAX_WIDTH}
+      margin={'auto'}
+      mt={['50px', '100px']}
+      spacing={4}
+      alignItems={'flex-start'}
+      padding={'50px'}
+    >
+      <Heading as="h2" size="xl">
+        {band.name}
+      </Heading>
+      <Detail title={BAND_DETAILS.country} detail={band.country} />
+      <Detail title={BAND_DETAILS.genre} detail={band.genre} />
+      <Detail title={BAND_DETAILS.year} detail={band.year} />
+      <Detail
+        title={BAND_DETAILS.members}
+        detail={band.members.map((member) => member.name).join(', ')}
+      />
+      <AlbumsTable bandId={band.id} />
+    </VStack>
   )
 }
